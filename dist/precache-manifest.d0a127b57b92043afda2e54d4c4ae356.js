9df34c28b57caf9d0522c12f14bdb7ee52fa45e8
Martinazzi <rafael.martinazzi@mobilesaude.com.br>
2021-01-26 08:42:56 -0300

    Deploy do site

---
+++ dist/precache-manifest.d0a127b57b92043afda2e54d4c4ae356.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e400aee4ab29cdc215a",
+    "url": "/css/app.f958f246.css"
+  },
+  {
+    "revision": "c4306d93fdcad6c94445",
+    "url": "/css/chunk-vendors.f4b25ac2.css"
+  },
+  {
+    "revision": "366d497d642391cd51e2091798a987c8",
+    "url": "/data.js"
+  },
+  {
+    "revision": "bfa5de3de05a6627e371b98ec11c3f1a",
+    "url": "/extends.js"
+  },
+  {
+    "revision": "ca08c1d5a695e0cf87f73b199c22f58a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e400aee4ab29cdc215a",
+    "url": "/js/app.6f4550cb.js"
+  },
+  {
+    "revision": "c4306d93fdcad6c94445",
+    "url": "/js/chunk-vendors.f6ce2d18.js"
+  },
+  {
+    "revision": "373e70106dd6f8b7fdc1681f1aa83d35",
+    "url": "/loki.js"
+  },
+  {
+    "revision": "8615c27e78548b4f1bb6a79e173b3104",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
